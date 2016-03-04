3b7782ff0d304baf314bf9c4009520d13bb82a85
fernandocardenasm <fernando.cardenasm@gmail.com>
2016-03-03 21:19:31 -0500

    Add method getRandomFact in FuncFact struct

--- FunFacts/FactModel.swift
+++ FunFacts/FactModel.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2016 Fernando. All rights reserved.
 //
 
+import GameKit
+
 struct FactModel {
     
     let facts = [
@@ -19,4 +21,10 @@ struct FactModel {
         "Some penguins can leap 2-3 meters out of the water.",
         "On average, it takes 66 days to form a new habit.",
         "Mammoths still walked the Earth when the Great Pyramid was being built." ]
+    
+    func getRandomFact() ->String{
+        let getRandomNumber = GKRandomSource.sharedRandom().nextIntWithUpperBound(facts.count)
+        
+        return facts[getRandomNumber]
+    }
 }
--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        funFactLabel.text = factModel.facts[0]
+        funFactLabel.text = factModel.getRandomFact()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,7 +28,7 @@ class ViewController: UIViewController {
 
     @IBAction func showFunFact() {
         
-        funFactLabel.text = factModel.facts[0]
+        funFactLabel.text = factModel.getRandomFact()
     }
     
 }
